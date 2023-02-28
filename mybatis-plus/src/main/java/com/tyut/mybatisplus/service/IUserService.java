637b1cec2063d11bf00590b05a99260bd285f6be
Algernomn <117904175+Algernomn@users.noreply.github.com>
2023-02-28 16:32:12 +0800

    Add files via upload
    add

---
+++ mybatis-plus/src/main/java/com/tyut/mybatisplus/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.tyut.mybatisplus.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tyut.mybatisplus.pojo.User;
+
+public interface IUserService extends IService<User> {
+}
---
+++ mybatis-plus/src/main/java/com/tyut/mybatisplus/service/UserService.java
@@ -0,0 +1,13 @@
+package com.tyut.mybatisplus.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tyut.mybatisplus.domain.User;
+
+/**
+* @author 13106
+* @description 针对表【user】的数据库操作Service
+* @createDate 2023-02-28 14:56:44
+*/
+public interface UserService extends IService<User> {
+
+}
