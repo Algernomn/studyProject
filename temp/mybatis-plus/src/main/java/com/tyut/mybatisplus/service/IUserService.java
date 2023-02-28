ad22e51ecfcccf00d7f3f4b8bd28939852441bec
Algernomn <117904175+Algernomn@users.noreply.github.com>
2023-02-28 16:31:11 +0800

    Delete temp directory

--- temp/mybatis-plus/src/main/java/com/tyut/mybatisplus/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.tyut.mybatisplus.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.tyut.mybatisplus.pojo.User;
-
-public interface IUserService extends IService<User> {
-}
